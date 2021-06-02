37a46ff304e6593128e97ab11f36011b0240ff2b
Martin Hennecke <hennecke@mbi-berlin.de>
2021-06-02 11:48:28 +0200

    fix for python3 compatibility

--- EpicsZeroDController.py
+++ EpicsZeroDController.py
@@ -24,7 +24,7 @@ class EpicsZeroDController(ZeroDController):
     def __init__(self, inst, props, *args, **kwargs):
         ZeroDController.__init__(self, inst, props, *args, **kwargs)
 
-        self.channels = [Channel(i + 1) for i in xrange(self.MaxDevice)]
+        self.channels = [Channel(i + 1) for i in range(self.MaxDevice)]
         self.read_channels = {}
 
     def AddDevice(self, ind):
